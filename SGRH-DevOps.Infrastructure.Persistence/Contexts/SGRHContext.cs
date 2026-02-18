6f60602e6657e5250a7369fb79dc6e5b91b63ea8
Erkyhanma <yenzel.bt@gmail.com>
2026-02-18 19:53:05 -0400

    feat: add Entity Framework Core setup

--- SGRH-DevOps.Infrastructure.Persistence/Contexts/SGRHContext.cs
+++ SGRH-DevOps.Infrastructure.Persistence/Contexts/SGRHContext.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using SGHR_DevOps.Entities.Hotel;
+using SGHR_DevOps.Entities.ReservationModule;
+using SGHR_DevOps.Entities.ServiceModule;
+using System.Reflection;
 
 namespace SGRH_DevOps.Infrastructure.Persistence.Contexts
 {
-    internal class SGRHContext
+    public class SGRHContext : DbContext
     {
+        public SGRHContext(DbContextOptions<SGRHContext> options) : base(options) { }
+
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Rate> Rates { get; set; }
+        public DbSet<Floor> Floors { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<RoomCategory> RoomCategories { get; set; }
+        public DbSet<Season> Seasons { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<ReservationService> ReservationServices { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        }
+
     }
 }
