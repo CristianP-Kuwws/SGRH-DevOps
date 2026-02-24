b738572492a38e55799639bb06f15795a415b9db
juanCabral-DJ <juandavidcabral71@gmail.com>
2026-02-24 06:27:18 -0700

    Aplicando MVC en la capa de presentacion y API Restful

---
+++ SGRH.API/Program.cs
@@ -0,0 +1,54 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+//implementar swagger
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "SGHR API",
+        Version = "v1",
+        Description = "API para gestion de reservas de hoteles",
+        Contact = new Microsoft.OpenApi.Models.OpenApiContact
+        {
+            Name = "SGRH"
+        }
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "SGRH API V1");
+    c.RoutePrefix = string.Empty; // Set Swagger UI at the app's root
+});
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
