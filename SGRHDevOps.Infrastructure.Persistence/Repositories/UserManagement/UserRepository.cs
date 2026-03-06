529dbdbe992c3c4ca090027a32340ddd9b434a59
Erkyhanma <yenzel.bt@gmail.com>
2026-03-06 12:49:38 -0400

    refactor: user management classes and repositories
    - Removed inheritance of `Admin`, `Client`, and `Recepcionist` from the `Person` class, which is now abstract.
    - Introduced the `User` class with properties for user management.
    - Replaced `IPersonRepository` with `IUserRepository` for user-specific operations.
    - Updated `ServiceRegistration` to include new user management services.
    - Created `UserRepository` to implement `IUserRepository`, replacing `PersonRepository`.
    - Add repositories to IOC

--- SGRHDevOps.Infrastructure.Persistence/Repositories/UserManagement/UserRepository.cs
+++ SGRHDevOps.Infrastructure.Persistence/Repositories/UserManagement/UserRepository.cs
@@ -1,12 +1,12 @@
-﻿using SGRHDevOps.Core.Domain.Common.Base;
+﻿using SGRHDevOps.Core.Domain.Entities.UserManagement;
 using SGRHDevOps.Core.Domain.Interfaces.UserManagement;
 using SGRHDevOps.Infrastructure.Persistence.Contexts;
 using SGRHDevOps.Infrastructure.Persistence.Repositories.Base;
 
 namespace SGRHDevOps.Infrastructure.Persistence.Repositories.UserManagement
 {
-    public class PersonRepository : GenericRepository<Person>, IPersonRepository
+    public class UserRepository : GenericRepository<User>, IUserRepository
     {
-        public PersonRepository(SGRHContext context) : base(context) { }
+        public UserRepository(SGRHContext context) : base(context) { }
     }
 }
