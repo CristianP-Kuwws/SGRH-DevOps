43c2dd9eb59ac6eb92b9fe7de756bc82231d8089
Yenzel Baez <142430567+ErkyHanma@users.noreply.github.com>
2026-03-06 18:49:17 -0400

    Feature/yenzel (#5)
    * Initial project structure and domain entities
    
    * Rename Core project folder to SGHR-DevOps.Core.Domain
    
    * Update project references
    
    * fix: Rename root namespaces and update project references
    
    - Updated root namespaces in multiple project files from underscores (`_`) to hyphens (`-`).
    
    * feat: add Entity Framework Core setup
    
    * refactor: change namespaces from SGHR_DevOps to SGRHDevOps
    
    This commit includes a comprehensive refactoring of the codebase, primarily focusing on renaming namespaces from `SGHR_DevOps` to `SGRHDevOps`. Multiple files across core domain, application, infrastructure, and shared components have been updated to reflect this change.
    
    * feat: add MailSettings and EmailService initial setup
    
    * refactor: removing unused directives
    
    * refactor: user management classes and repositories
    
    - Removed inheritance of `Admin`, `Client`, and `Recepcionist` from the `Person` class, which is now abstract.
    - Introduced the `User` class with properties for user management.
    - Replaced `IPersonRepository` with `IUserRepository` for user-specific operations.
    - Updated `ServiceRegistration` to include new user management services.
    - Created `UserRepository` to implement `IUserRepository`, replacing `PersonRepository`.
    - Add repositories to IOC
    
    * refactor: remove old MSTests projects to a xUnitTest Project
    
    - feat: add SGRHDevOps.Unit.Tests and SGRHDevOps.Integration.Tests projects
    
    ---------
    
    Co-authored-by: Cristian Pacheco <pachecocruzc258@gmail.com>

--- SGRHDevOps.Core.Domain/Interfaces/UserManagement/IUserRepository.cs
+++ SGRHDevOps.Core.Domain/Interfaces/UserManagement/IUserRepository.cs
@@ -1,9 +1,9 @@
-﻿using SGRHDevOps.Core.Domain.Common.Base;
+﻿using SGRHDevOps.Core.Domain.Entities.UserManagement;
 using SGRHDevOps.Core.Domain.Interfaces.Base;
 
 namespace SGRHDevOps.Core.Domain.Interfaces.UserManagement
 {
-    public interface IPersonRepository : IGenericRepository<Person>
+    public interface IUserRepository : IGenericRepository<User>
     {
 
     }
--- SGRHDevOps.Infrastructure.Persistence/IOC/ServiceRegistration.cs
+++ SGRHDevOps.Infrastructure.Persistence/IOC/ServiceRegistration.cs
@@ -1,7 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SGRHDevOps.Core.Domain.Interfaces.Hotel;
+using SGRHDevOps.Core.Domain.Interfaces.ReservationModule;
+using SGRHDevOps.Core.Domain.Interfaces.ServiceModule;
+using SGRHDevOps.Core.Domain.Interfaces.UserManagement;
 using SGRHDevOps.Infrastructure.Persistence.Contexts;
+using SGRHDevOps.Infrastructure.Persistence.Repositories.Hotel;
+using SGRHDevOps.Infrastructure.Persistence.Repositories.ReservationModule;
+using SGRHDevOps.Infrastructure.Persistence.Repositories.ServiceModule;
+using SGRHDevOps.Infrastructure.Persistence.Repositories.UserManagement;
 
 namespace SGRHDevOps.Infrastructure.Persistence.IOC
 {
@@ -38,6 +46,16 @@ namespace SGRHDevOps.Infrastructure.Persistence.IOC
 
 
             #region Repositories
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IRoleRepository, RoleRepository>();
+            services.AddScoped<IFloorRepository, FloorRepository>();
+            services.AddScoped<IRateRepository, RateRepository>();
+            services.AddScoped<IRoomCategoryRepository, RoomCategoryRepository>();
+            services.AddScoped<IRoomRepository, RoomRepository>();
+            services.AddScoped<ISeasonRepository, SeasonRepository>();
+            services.AddScoped<IReservationRepository, ReservationRepository>();
+            services.AddScoped<IReservationServiceRepository, ReservationServiceRepository>();
+            services.AddScoped<IServiceRepository, ServiceRepository>();
             #endregion
 
 
