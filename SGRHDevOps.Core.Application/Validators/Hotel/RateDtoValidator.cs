61115ab80d332149508da0d07e56410f28479239
Cristian Pacheco <pachecocruzc258@gmail.com>
2026-03-06 21:35:49 -0400

    feat: add FluentValidation validators for Rate and Room DTOs

---
+++ SGRHDevOps.Core.Application/Validators/Hotel/RateDtoValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using SGRHDevOps.Core.Application.Dtos.Hotel;
+
+namespace SGRHDevOps.Core.Application.Validators.Hotel
+{
+    public class RateDtoValidator : AbstractValidator<RateDto>
+    {
+        public RateDtoValidator()
+        {
+            RuleFor(x => x.CategoryId)
+                .GreaterThan(0).WithMessage("CategoryId must be greater than zero.");
+
+            RuleFor(x => x.SeasonId)
+                .GreaterThan(0).WithMessage("SeasonId must be greater than zero.");
+
+            RuleFor(x => x.NightPrice)
+                .GreaterThan(0).WithMessage("NightPrice must be greater than zero.");
+        }
+    }
+}
